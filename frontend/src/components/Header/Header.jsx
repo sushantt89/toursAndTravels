84d33bd0e78a1bfe855a373fd923bf5e0757e495
sushantworks <sushant.maharjan@ascend.com.np>
2024-06-03 21:33:49 +0545

    review posting completed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
-import React,{useRef,useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import { Container, Row, Button } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
-import './header.css';
+import "./header.css";
+
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const nav_links = [
   {
@@ -18,32 +22,43 @@ const nav_links = [
     display: "Tours",
   },
 ];
-const Header = () => {
-  const headerRef =useRef(null)
-  const stickyHeaderFunc=()=>{
-    window.addEventListener('scroll',()=>{
-      if(document.body.scrollTop>80 || document.documentElement.scrollTop>80){
-        headerRef.current.classList.add('sticky_header')
-      }
-      else{
-        headerRef.current.classList.remove('sticky_header')
 
+const Header = () => {
+  const navigate = useNavigate();
+  const { dispatch, user } = useContext(AuthContext);
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+  };
+
+  const headerRef = useRef(null);
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky_header");
+      } else {
+        headerRef.current.classList.remove("sticky_header");
       }
-    })
-  }
-  useEffect(()=>{
+    });
+  };
+  useEffect(() => {
     stickyHeaderFunc();
-    return window.removeEventListener("scroll",stickyHeaderFunc)
-  })
+    return window.removeEventListener("scroll", stickyHeaderFunc);
+  });
   return (
     <header className="header" ref={headerRef}>
       <Container>
         <Row>
-        
           <div className="nav_wrapper d-flex aligh-items-center justify-content-between">
             {/* ============== logo start ============= */}
             <div className="logo">
-              <img src={logo} alt="logo" />
+              <Link to={"/"}>
+                <img src={logo} alt="logo" />{" "}
+              </Link>
             </div>
             {/* ============== logo end ============= */}
             {/* ============== menu start ============= */}
@@ -51,7 +66,14 @@ const Header = () => {
               <ul className="menu d-flex align-items-center gap-5">
                 {nav_links.map((item, index) => (
                   <li className="nav_item" key={index}>
-                    <NavLink to={item.path} className={navClass=>navClass.isActive?'active_link':''}>{item.display}</NavLink>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active_link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -59,15 +81,26 @@ const Header = () => {
             {/* ============== menu end ============= */}
             <div className="nav_right d-flex align-items-center gap-4">
               <div className="nav_btns d-flex align-items-center gap-4">
-                <Button className="btn secondary_btn">
-                  <Link to="/login">Login</Link>
-                </Button>
-                <Button className="btn primary_btn">
-                  <Link to="/register">Register</Link>
-                </Button>
+                {user ? (
+                  <>
+                    <h6 className="mb-0">{user.username}</h6>
+                    <Button className="btn primary_btn mb-0" onClick={logout}>
+                      Logout
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Button className="btn secondary_btn">
+                      <Link to="/login">Login</Link>
+                    </Button>
+                    <Button className="btn primary_btn">
+                      <Link to="/register">Register</Link>
+                    </Button>
+                  </>
+                )}
               </div>
               <span className="mobile_menu">
-              <i className="ri-menu-line"></i>
+                <i className="ri-menu-line"></i>
               </span>
             </div>
           </div>
--- frontend/src/components/pages/Tours.jsx
+++ frontend/src/components/pages/Tours.jsx
@@ -3,16 +3,37 @@ import { Container, Row, Col } from "reactstrap";
 import CommonSection from "../../shared/CommonSection";
 import "../../styles/tour.css";
 import Newsletter from "../../shared/Newsletter";
-import tourData from "../../assets/data/tours";
 import TourCard from "../../shared/TourCard";
 import SearchBar from "../../shared/SearchBar";
+import useFetch from "../../hooks/useFetch";
+import { BASE_URL } from "../../utils/config";
+import Loader from "../../shared/Loader";
+
 const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
+
+  const { data: allTours, loading, error } = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const { data: tourCount, loading: countLoading, error: countError } = useFetch(`${BASE_URL}/tours/search/getToursCount`);
+
   useEffect(() => {
-    const pages = Math.ceil(5 / 4); //later to use backend data to count
-    setPageCount(pages);
+    if (tourCount !== undefined) {
+      const pages = Math.ceil(tourCount / 8); // Display 8 tours per page
+      setPageCount(pages);
+    }
+  }, [tourCount,page,allTours]);
+  useEffect(() => {
+    const scrollDelay = 1100;
+
+    const timer = setTimeout(() => {
+      // Scroll to 400 pixels down from the top with smooth behavior
+      window.scrollTo({ top: 420, behavior: 'smooth' });
+      
+    }, scrollDelay);
+    return () => clearTimeout(timer);
   }, [page]);
+  
+
   return (
     <>
       <CommonSection title={"All Tours"} />
@@ -25,29 +46,33 @@ const Tours = () => {
       </section>
       <section className="pt-0">
         <Container>
-          <Row>
-            {tourData?.map((tour) => (
-              <Col lg="3" className="mb-4" key={tour.id}>
-                <TourCard tour={tour} />
+          {(loading || countLoading) && <Loader />}
+          {(error || countError) && <h4>{error || countError}</h4>}
+          {!loading && !error && !countLoading && !countError && (
+            <Row>
+              {allTours?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour.id}>
+                  <TourCard tour={tour} />
+                </Col>
+              ))}
+              <Col lg="12">
+                <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                  {[...Array(pageCount).keys()].map((number) => (
+                    <span
+                      key={number}
+                      onClick={() => setPage(number)}
+                      className={page === number ? "active_page" : ""}
+                    >
+                      {number + 1}
+                    </span>
+                  ))}
+                </div>
               </Col>
-            ))}
-            <Col lg="12">
-              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-                {[...Array(pageCount).keys()].map((number) => (
-                  <span
-                    key={number}
-                    onClick={() => setPage(number)}
-                    className={page === number ? "active_page" : ""}
-                  >
-                    {number + 1}
-                  </span>
-                ))}
-              </div>
-            </Col>
-          </Row>
+            </Row>
+          )}
         </Container>
       </section>
-      <Newsletter/>
+      <Newsletter />
     </>
   );
 };
